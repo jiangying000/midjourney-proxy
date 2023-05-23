96c4e2c07b7a984e69462ce39560396bc7912de9
jiangying <krivergo3@gmail.com>
2023-05-23 21:24:17 +0800

    disable security

--- src/main/java/com/github/novicezk/midjourney/SecurityConfig.java
+++ src/main/java/com/github/novicezk/midjourney/SecurityConfig.java
@@ -1,77 +1,29 @@
-package com.github.novicezk.midjourney;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("leo")
-                .password(passwordEncoder.encode("leoPass"))
-                .roles("USER");
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("jy")
-                .password(passwordEncoder.encode("jyAuth000"))
-                .roles("USER");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/mj/**")
-                .authenticated()
-                .and()
-                .csrf()
-                .disable()
-                .httpBasic();
-    }
-}
+//package com.github.novicezk.midjourney;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
 //@Configuration
 //@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+////public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication();
-////                .passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance())
-////                .withUser("leo")
-////                .password(new BCryptPasswordEncoder().encode("leoPass"))
-////                .password("{noop}leoPass")
-////                .roles("USER");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/mj/**")
-//                .authenticated()
-//                .and()
-//                .httpBasic().and().csrf().disable();
-//    }
-//}
-
-
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 //        auth.inMemoryAuthentication()
+//                .passwordEncoder(passwordEncoder)
 //                .withUser("leo")
-//                .password(encoder.encode("leoPass"))
+//                .password(passwordEncoder.encode("leoPass@teamgpt!&"))
+//                .roles("USER");
+//        auth.inMemoryAuthentication()
+//                .passwordEncoder(passwordEncoder)
+//                .withUser("jy")
+//                .password(passwordEncoder.encode("jyAuth000"))
 //                .roles("USER");
 //    }
 //
@@ -81,83 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .antMatchers("/mj/**")
 //                .authenticated()
 //                .and()
+//                .csrf()
+//                .disable()
 //                .httpBasic();
 //    }
 //}
-
-//@Configuration
-//@EnableWebSecurity
-//public class CustomWebSecurityConfigurerAdapter {
-//
-//
-//    @Autowire
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user1")
-//                .password(passwordEncoder().encode("user1Pass"))
-//                .authorities("ROLE_USER");
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/securityNone")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
-////                .authenticationEntryPoint(authenticationEntryPoint);
-////        http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
-////        http.addFilter(BasicAuthenticationFilter.class);
-////        return http.build();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
-//
-
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig
-//{
-////    @Autowired
-////    private AppBasicAuthenticationEntryPoint authenticationEntryPoint;
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/mj/**")
-////                .permitAll()
-////                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic().and().csrf().disable();
-////        http.authorizeRequests().antMatchers().authenticated()
-////                .authenticationEntryPoint(authenticationEntryPoint);
-//        return http.build();
-//    }
-//
-//    @Bean
-////    public InMemoryUserDetailsManager userDetailsService() {
-//    public InMemoryUserDetailsManager userDetailsService() {
-//        UserDetails user = User
-//                .withUsername("leo")
-//                .password(passwordEncoder().encode("leoPass"))
-//                .roles("USER_ROLE")
-//                .build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder(8);
-//    }
-//}
-//
-//
-
